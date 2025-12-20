ca436b57ec840033fc31dae9f56147f26f0a9c04
sontungN <s3979348@rmit.edu.vn>
2025-12-20 22:16:58 +0700

    feat: initialize the project structure with Spring Boot, Maven wrapper, api/service modules, and basic configuration files

---
+++ SkillTagService/src/main/java/rmit/saintgiong/tagservice/JmSkillTagApplication.java
@@ -0,0 +1,13 @@
+package rmit.saintgiong.tagservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmSkillTagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmSkillTagApplication.class, args);
+    }
+
+}
---
+++ SkillTagService/src/test/java/rmit/saintgiong/tagservice/SkillTagServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rmit.saintgiong.tagservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillTagServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
